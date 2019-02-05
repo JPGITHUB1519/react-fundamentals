43cf8d7c2607edaa19fff557ce14408ce508c192
Jean Urena <juanpedro1519@gmail.com>
2019-02-05 02:57:52 -0300

    L11 - Composition via props.children

--- github-battle/app/utils/api.js
+++ github-battle/app/utils/api.js
@@ -5,14 +5,14 @@ var sec = "YOUR_SECRET_ID";
 var params = "?client_id=" + id + "&client_secret=" + sec;
 
 function getProfile(username) {
-    return axios.get('https://api.github.com/users' + username + params)
+    return axios.get('https://api.github.com/users/' + username + params)
         .then(function(user) {
             return user.data;
         });
 }
 
 function getRepos(username) {
-    return axios.get('https://api.github.com/users' + username + '/repos/' + params + '&per_page=100');
+    return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100');
 }
 
 function getStartCount(repos) {
@@ -38,6 +38,7 @@ function getUserData(player) {
        getProfile(player),
        getRepos(player)
    ]).then(function(data) {
+       console.log(data);
        var profile = data[0];
        var repos = data[1];
 
